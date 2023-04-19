9c77a71bc4d0e1665205734d9dafea6a5ca58a32
祐陽 石川 <OIBC34310@ojs.ac.jp>
2023-04-19 11:56:17 +0900

    複数のボールを表示

--- Sample/BallApp/Program.cs
+++ Sample/BallApp/Program.cs
@@ -13,12 +13,15 @@ namespace BallApp {
         private SoccerBall soccerBall;
         private PictureBox pb;  //画像を表示するコントロール
 
+        private List<SoccerBall> balls = new List<SoccerBall>();    //ボールインスタンス格納用
+        private List<PictureBox> pbs = new List<PictureBox>();      //表示用
+
         static void Main(string[] args) {
             Application.Run(new Program());
         }
         public Program() { 
             this.Size = new Size(800, 600);
-            this.BackColor = Color.Green;
+            this.BackColor = Color.Aqua;
             this.Text = "BallGame";
             this.MouseClick += Program_MouseClick;
 
@@ -38,12 +41,20 @@ namespace BallApp {
             pb.SizeMode = PictureBoxSizeMode.StretchImage;  //画像の表示モード
             pb.Parent = this;
 
+            balls.Add(soccerBall);
+            pbs.Add(pb);
+
             moveTimer.Start();//タイマースタート
         }
         //タイマータイムアウト時のイベントハンドラー
         private void MoveTimer_Tick(object sender, EventArgs e) {
-            soccerBall.Move();//移動
-            pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);//画像の位置
+            for (int i = 0; i < balls.Count; i++)
+            {
+                balls[i].Move();//移動
+                pbs[i].Location = new Point((int)balls[i].PosX, (int)balls[i].PosY);//画像の位置
+
+            }
+
         }
     }
 }
