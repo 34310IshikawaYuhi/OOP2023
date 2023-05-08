712579e2f527f57a2e64c73671791555f70a1bc1
祐陽 石川 <OIBC34310@ojs.ac.jp>
2023-05-08 11:44:18 +0900

    0508演習１

--- Chapter01/Product/Program.cs
+++ Chapter01/Product/Program.cs
@@ -8,12 +8,26 @@ namespace ProductSample {
 
     class Program {
         static void Main(string[] args) {
+            #region P２６のサンプルプログラム
             //インスタンス生成
-            Product karinto = new Product(123, "かりんとう", 180);
-            Product daifuku = new Product(235, "大福もち", 160);
+            //Product karinto = new Product(123, "かりんとう", 180);
+            //Product daifuku = new Product(235, "大福もち", 160);
 
-            Console.WriteLine("税込み価格" + karinto.GetPriceIncludingTax());
-            Console.WriteLine("税込み価格" + daifuku.GetPriceIncludingTax());
+            //Console.WriteLine("税込み価格" + karinto.GetPriceIncludingTax());
+            //Console.WriteLine("税込み価格" + daifuku.GetPriceIncludingTax());
+            #endregion
+
+            //DateTime date = new DateTime(2023, 5, 8);
+            DateTime date = DateTime.Today;
+            Console.WriteLine("今日の日付：" + date.ToString("yyyy年MM月dd日"));
+
+            //10日後を求める
+            DateTime daysAfter10 = date.AddDays(10);
+            Console.WriteLine("10日後:" + daysAfter10.ToString("yyyy年MM月dd日"));
+
+            //10日前を求める
+            DateTime daybefore10 = date.AddDays(-10);
+            Console.WriteLine("10日前:" + daybefore10.ToString("yyyy年MM月dd日"));
         }
     }
 }
