2c48223f42a4350c974bb9e6515800bbf27f8de0
祐陽 石川 <OIBC34310@ojs.ac.jp>
2023-05-22 10:14:13 +0900

    p79問2.3完成

--- Chapter02/演習問題/Exercise03/Program.cs
+++ Chapter02/演習問題/Exercise03/Program.cs
@@ -7,6 +7,12 @@ using System.Threading.Tasks;
 namespace Exercise03 {
     class Program {
         static void Main(string[] args) {
+            var sales = new SalesCounter(@"data\sales.csv");
+            var amountPerStore = sales.GetPerStoreSales();
+            foreach (var obj in amountPerStore) {
+                Console.WriteLine("{0} {1:C}", obj.Key, obj.Value);
+
+            }
         }
     }
 }
