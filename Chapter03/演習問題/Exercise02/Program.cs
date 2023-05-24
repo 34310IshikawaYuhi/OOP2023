e5fcd403fef5ef4e8963fd9ab515b7f109f4ef8f
祐陽 石川 <OIBC34310@ojs.ac.jp>
2023-05-24 12:07:42 +0900

    p100問3.2の回答例

--- Chapter03/演習問題/Exercise02/Program.cs
+++ Chapter03/演習問題/Exercise02/Program.cs
@@ -25,26 +25,32 @@ namespace Exercise02 {
                 var city = Console.ReadLine();
                 if (string.IsNullOrEmpty(city))
                     break;
+
                 var index = names.FindIndex(n => n == city);
                 Console.WriteLine(index);
-
             } while (true);
         }
 
         private static void Exercise2_2(List<string> names) {
-            var cnt = names.Where(n => n.Contains("o")).Count();
+            //var cnt = names.Where(n => n.Contains("o")).Count();
+            var cnt = names.Count(n => n.Contains("o"));
             Console.WriteLine(cnt);
         }
 
         private static void Exercise2_3(List<string> names) {
-            var list = names.Where(n => n.Contains("o"));
+            var list = names.Where(n => n.Contains("o")).ToArray();
             foreach (var city in list) {
                 Console.WriteLine(city);
             }
         }
 
         private static void Exercise2_4(List<string> names) {
-        
+            var selected = names.Where(n => n.StartsWith("B"))
+                                .Select(n=>new {n,n.Length});
+
+            foreach (var item in selected) {
+                Console.WriteLine("{0},{1}", item.n, item.Length);
+            }
         }
     }
 }
