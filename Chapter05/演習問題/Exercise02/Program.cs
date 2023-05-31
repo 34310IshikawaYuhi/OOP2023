0cd3c66f0da6d17f6aab4220b97e3302ed77d075
祐陽 石川 <OIBC34310@ojs.ac.jp>
2023-05-31 11:48:05 +0900

    p158 問5.1完成

---
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("文字列１：");
+            var s1 = Console.ReadLine();
+
+            Console.Write("文字列２：");
+            var s2 = Console.ReadLine();
+        }
+    }
+}
