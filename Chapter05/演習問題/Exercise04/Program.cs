15fc562808a426595fb556b9b14ed0d489161089
祐陽 石川 <OIBC34310@ojs.ac.jp>
2023-06-06 09:44:31 +0900

    問5.4完成

--- Chapter05/演習問題/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Program.cs
@@ -7,8 +7,19 @@ using System.Threading.Tasks;
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
-            var text = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+            var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
 
+            string[] s = line.Split(';');
+            string[] replaced = { "", "", "" };
+           
+            replaced[0] = s[0].Replace("Novelist=", "作家　:");
+            replaced[1] = s[1].Replace("BestWork=", "代表作:");
+            replaced[2] = s[2].Replace("Born=", "誕生年:");
+
+            foreach (var novel in replaced) {
+                Console.WriteLine(novel);
+            }
+             
         }
     }
 }
