123954bb6a4491f7126a672d34ea0d5c80496477
祐陽 石川 <OIBC34310@ojs.ac.jp>
2023-06-07 10:53:25 +0900

    LINQのメモリ使用量と実行時間

---
+++ Chapter06/Section02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section02 {
+    class Program {
+        static void Main(string[] args) {
+            Stopwatch sw = new Stopwatch();//タイマーインスタンス生成
+            sw.Start();//タイマースタート
+
+            var numbers = Enumerable.Range(1, 1000000);
+            WriteTotalMemory("配列確保後");
+
+            //偶数だけ取り出す
+            var evenNumbers = numbers.Where(x => x % 2 == 0);
+            WriteTotalMemory("偶数抽出後");
+            /*foreach (var item in evenNumbers) {
+                Console.WriteLine(item);
+            }*/
+
+            //取り出した偶数の平均値を求める
+            var ave = evenNumbers.Average();
+            WriteTotalMemory("偶数抽出後平均");
+
+           Console.WriteLine("実行時間 = {0}", sw.Elapsed.ToString(@"ss\.fffff"));//時間表示
+        }
+
+        static void WriteTotalMemory(string header) {
+            var totalMemory = GC.GetTotalMemory(true) / 1024.0 / 1024.0;
+            Console.WriteLine($"{header}: {totalMemory:0.000 MB}");
+        }
+    }
+}
