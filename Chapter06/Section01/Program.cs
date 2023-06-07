0bb4451951e7aff30f47fbf7c88eeed59d598a05
祐陽 石川 <OIBC34310@ojs.ac.jp>
2023-06-07 09:59:42 +0900

    オブジェクトのLINQ利用

---
+++ Chapter06/Section01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class Program {
+        static void Main(string[] args) {
+            var numbers = new List<int> { 9, 7, 5, 4, 2, 5, 4, 0, 4, 1, 0, 4, };
+            Console.WriteLine(numbers.Average());
+
+            var books = Books.GetBooks();
+
+            var booksOBJ = books.Where(x => x.Title.Contains("物語")).Average(x => x.Pages);
+            Console.WriteLine("平均:{0}円",booksOBJ);
+
+            var longTitleBooks = books.OrderByDescending(x=> x.Title.Length);
+            foreach (var book in longTitleBooks) {
+                Console.WriteLine("{0}:{1}", book.Title, book.Price);
+            }
+        }
+    }
+}
