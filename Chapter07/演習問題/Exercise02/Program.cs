baf1df387ee279f3013db97b18a7eba9fb20ab1d
祐陽 石川 <OIBC34310@ojs.ac.jp>
2023-06-29 12:10:04 +0900

    Exercise03

---
+++ Chapter07/演習問題/Exercise02/Program.cs
@@ -0,0 +1,25 @@
+﻿using Section03;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            //コンストラクタ呼び出し
+            var abbrs = new Abbreviations();
+
+            //Addメソッド
+            abbrs.Add("IOC", "国際オリンピック委員会");
+            abbrs.Add("NPT", "核拡散防止条約");
+
+            //7.2.3
+            //上のAddメソッドで、２つのオブジェクトを追加しているので読み込んだ単語
+
+            //7.2.3(Removeの呼び出し)
+            abbrs.Remove("IOC");
+        }
+    }
+}
