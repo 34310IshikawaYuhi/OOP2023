2f138574d6522c375ca8b0a787f1f1a776abc448
祐陽 石川 <OIBC34310@ojs.ac.jp>
2023-07-03 15:36:45 +0900

    カレンダーアプリ　日数計算　完成

---
+++ Chapter08/フォームアプリケーション/CalenderApp/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace CalenderApp {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.btDayCalc = new System.Windows.Forms.Button();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "日付：";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.dtpDate.Location = new System.Drawing.Point(86, 12);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(200, 31);
+            this.dtpDate.TabIndex = 1;
+            // 
+            // btDayCalc
+            // 
+            this.btDayCalc.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btDayCalc.Location = new System.Drawing.Point(16, 70);
+            this.btDayCalc.Name = "btDayCalc";
+            this.btDayCalc.Size = new System.Drawing.Size(127, 54);
+            this.btDayCalc.TabIndex = 2;
+            this.btDayCalc.Text = "日数計算";
+            this.btDayCalc.UseVisualStyleBackColor = true;
+            this.btDayCalc.Click += new System.EventHandler(this.btDayCalc_Click);
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Location = new System.Drawing.Point(16, 146);
+            this.tbMessage.Multiline = true;
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(291, 244);
+            this.tbMessage.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 450);
+            this.Controls.Add(this.tbMessage);
+            this.Controls.Add(this.btDayCalc);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "カレンダーアプリ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.Button btDayCalc;
+        private System.Windows.Forms.TextBox tbMessage;
+    }
+}
+
---
+++ Chapter08/フォームアプリケーション/CalenderApp/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalenderApp {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void btDayCalc_Click(object sender, EventArgs e) {
+            var dtp = dtpDate.Value;
+            var now = DateTime.Now;
+
+            var daySpan = now - dtp;
+
+
+            tbMessage.Text = daySpan.ToString("dd");
+        }
+    }
+}
