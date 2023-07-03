ace1229ecdb4fbfe4e163ebb6460f1d58510b421
祐陽 石川 <OIBC34310@ojs.ac.jp>
2023-07-03 14:16:44 +0900

    問8.2　完成例

---
+++ Chapter08/演習問題/Exercise02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+
+            var dt = DateTime.Now;
+
+            foreach (var dayofweek in Enum.GetValues(typeof(DayOfWeek))) {
+                Console.Write("{0:yy/MM/dd}の次週の{1} : ", dt,dayofweek);
+                Console.WriteLine("{0:yy/MM/dd(ddd)}",NextWeek(dt,(DayOfWeek)dayofweek));
+            }
+        }
+        public static  DateTime NextWeek(DateTime date , DayOfWeek dayOfWeek) {
+            var nextweek = date.AddDays(7);
+            var days = (int)dayOfWeek - (int)(date.DayOfWeek);
+            return nextweek.AddDays(days);
+       
+        }
+    }
+}
