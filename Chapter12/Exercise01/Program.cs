8c84c445f4863af7262130fd6337b703a3617ba5
Yuhi Ishikawa <OIBC34310@ojs.ac.jp>
2023-09-04 15:57:33 +0900

    12-1　未完成

---
+++ Chapter12/Exercise01/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+
+            Exercise1_1("employee.xml");
+
+            // これは確認用
+            Console.WriteLine(File.ReadAllText("employee.xml"));
+            Console.WriteLine();
+
+            Exercise1_2("employees.xml");
+            Exercise1_3("employees.xml");
+            Console.WriteLine();
+
+            Exercise1_4("employees.json");
+
+            // これは確認用
+            Console.WriteLine(File.ReadAllText("employees.json"));
+        }
+
+        private static void Exercise1_1(string v) {
+
+            using(var writer = XmlWriter.Create("employee.xml")) {
+                var serializer = new XmlSerializer(employee.GetType());
+                serializer.Serialize(writer, employee);
+            }
+            using (var reader = XmlReader.Create("employee.xml")) {
+                var serializer = new XmlSerializer(typeof(Employee));
+                var employee = serializer.Deserialize(reader) as Employee;
+                Console.WriteLine(employee);
+            }
+        
+        }
+
+        private static void Exercise1_2(string v) {
+        
+        
+        }
+
+        private static void Exercise1_3(string v) {
+        
+        
+        }
+
+        private static void Exercise1_4(string v) {
+        
+        
+        }
+    }
+}
