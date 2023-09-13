d3c90dfb2468554f9ec63096b087d79e3cd074c0
Yuhi Ishikawa <OIBC34310@ojs.ac.jp>
2023-09-13 09:23:56 +0900

    P329まで

--- Chapter13/SampleEntityFramework/Program.cs
+++ Chapter13/SampleEntityFramework/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SampleEntityFramework.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,36 @@ using System.Threading.Tasks;
 namespace SampleEntityFramework {
     class Program {
         static void Main(string[] args) {
+            InsertBooks();
+            Console.Write("データを挿入しました。続けるにはEnterキーを押してください。");
+            Console.ReadLine();
+            Console.WriteLine();
+        }
+        // List 13-5
+        static void InsertBooks() {
+            using (var db = new BooksDbContext()) {
+                var book1 = new Book {
+                    Title = "坊ちゃん",
+                    PublishedYear = 2003,
+                    Author = new Author {
+                        Birthday = new DateTime(1867, 2, 9),
+                        Gender = "M",
+                        Name = "夏目漱石",
+                    }
+                };
+                db.Books.Add(book1);
+                var book2 = new Book {
+                    Title = "人間失格",
+                    PublishedYear = 1990,
+                    Author = new Author {
+                        Birthday = new DateTime(1909, 6, 19),
+                        Gender = "M",
+                        Name = "太宰治",
+                    }
+                };
+                db.Books.Add(book2);
+                db.SaveChanges();
+            }
         }
     }
 }
