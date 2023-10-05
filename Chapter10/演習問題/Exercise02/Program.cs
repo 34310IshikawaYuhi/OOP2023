723321873264f3b38937325270c2d901887de051
Yuhi Ishikawa <OIBC34310@ojs.ac.jp>
2023-10-05 10:04:50 +0900

    Chapter10 演習問題2完成

---
+++ Chapter10/演習問題/Exercise02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            Pickup3DigitNumber("sample.txt");
+        }
+
+        private static void Pickup3DigitNumber(string file) {
+
+            var lists = new List<string>();
+            var regex = new Regex(@"^[0-9]{3,}$");
+
+            foreach (var line in File.ReadLines(file)) {
+
+                string[] str = line.Split(' ');
+                lists.AddRange(str);
+
+            }
+            foreach (var list in lists) {
+
+                var isMatch = regex.IsMatch(list);
+                if (isMatch)
+                    Console.WriteLine(list);
+
+            }
+
+
+        }
+    }
+}
---
+++ Chapter10/演習問題/Exercise03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    class Program {
+        static void Main(string[] args) {
+            var texts = new[] {
+               "Time is money.",
+               "What time is it?",
+               "It will take time.",
+               "We reorganized the timetable.",
+            };
+
+
+
+
+        }
+    }
+}
