ac8359b065f044c35a2f2340d166bc420043679c
Yuhi Ishikawa <OIBC34310@ojs.ac.jp>
2023-10-10 10:03:00 +0900

    chapter10演習問題4 解答例

--- Chapter10/演習問題/Exercise04/Program.cs
+++ Chapter10/演習問題/Exercise04/Program.cs
@@ -3,20 +3,19 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
             var lines = File.ReadAllLines("sample.txt");
-            var pattern = @"[Vv]ersion";
-            foreach (var line in lines) {
 
-            }
+            var newline = lines
+                .Select(s => Regex.Replace(s, @"\b(v|V)ersion\s*=\s*""v4.0""", @"version=""5.0"" "));
 
-
-
-
+            //書き込み
+            File.WriteAllLines("Sample.txt",newline);
 
             // これ以降は確認用
             var text = File.ReadAllText("sample.txt");
