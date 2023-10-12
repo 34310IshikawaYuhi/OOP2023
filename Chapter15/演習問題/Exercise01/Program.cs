78851ef8ef5854634e775c7491c72daca15c6125
Yuhi Ishikawa <OIBC34310@ojs.ac.jp>
2023-10-12 11:06:16 +0900

    15章演習問題1-5完成

--- Chapter15/演習問題/Exercise01/Program.cs
+++ Chapter15/演習問題/Exercise01/Program.cs
@@ -57,7 +57,15 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_5() {
-
+            var books = Library.Books.Where(b => b.PublishedYear == 2016)
+                                        .Join(Library.Categories,
+                                              book => book.CategoryId,
+                                              category => category.Id,
+                                              (book, category) => category.Name)
+                                        .Distinct();
+            foreach (var name in books) {
+                Console.WriteLine(name);
+            }
         }
 
         private static void Exercise1_6() {
