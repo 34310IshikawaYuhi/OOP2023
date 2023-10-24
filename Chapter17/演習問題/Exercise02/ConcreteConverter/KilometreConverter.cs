54367ad8cd1d655ee7e05be2441bba27ccd4a4d2
Yuhi Ishikawa <OIBC34310@ojs.ac.jp>
2023-10-24 11:55:15 +0900

    WPFのサンプルプログラム１

---
+++ Chapter17/演習問題/Exercise02/ConcreteConverter/KilometreConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02.ConcreteConverter {
+    class KilometreConverter : Framework.ConverterBase{
+        public override bool IsMyUnit(string name) {
+            return name.ToLower() == "kilometre" || name == UnitName;
+        }
+        protected override double Ratio { get { return 1000; } }
+        public override string UnitName { get { return "キロメートル"; } }
+    }
+}
---
+++ Chapter17/演習問題/Exercise02/ConcreteConverter/MileConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02.ConcreteConverter {
+    class MileConverter : Framework.ConverterBase {
+        public override bool IsMyUnit(string name) {
+            return name.ToLower() == "mile" || name == UnitName;
+        }
+        protected override double Ratio { get { return 1609.344; } }
+        public override string UnitName { get { return "マイル"; } }
+    }
+}
