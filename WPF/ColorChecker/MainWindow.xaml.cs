b5c820418a08868bec7d4044e85c3e1b59623116
Yuhi Ishikawa <OIBC34310@ojs.ac.jp>
2023-11-01 11:21:56 +0900

    ColorChecker　ストックボタン完成

--- WPF/ColorChecker/MainWindow.xaml.cs
+++ WPF/ColorChecker/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Reflection;
 
 namespace ColorChecker {
     /// <summary>
@@ -20,6 +21,38 @@ namespace ColorChecker {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            DataContext = GetColorList();
         }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            setColor();
+        }
+
+        private void setColor() {
+            Color color = Color.FromRgb((byte)rSlider.Value, (byte)gSlider.Value , (byte)bSlider.Value);
+            ColorArea.Background = new SolidColorBrush(color);
+        }
+
+        private void stockButton_Click(object sender, RoutedEventArgs e) {
+            stockList.Items.Add($"R:{rValue.Text} G:{gValue.Text} B:{bValue.Text}");
+        }
+
+        private MyColor[] GetColorList() {
+            return typeof(Colors).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                .Select(i => new MyColor() { Color = (Color)i.GetValue(null), Name = i.Name }).ToArray();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            var selectColor = (MyColor)((ComboBox)sender).SelectedItem;
+            rValue.Text = selectColor.Color.R.ToString();
+            gValue.Text = selectColor.Color.G.ToString();
+            bValue.Text = selectColor.Color.B.ToString();
+        }
+    }
+
+
+    public class MyColor {
+        public Color Color { get; set; }
+        public string Name { get; set; }
     }
 }
